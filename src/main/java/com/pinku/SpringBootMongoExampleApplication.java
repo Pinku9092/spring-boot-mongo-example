caf8d09bff1c89b060daf423351b40c1ea254a47
pinku <kpinku32@gmail.com>
2025-04-03 12:11:09 +0530

    added video url

--- src/main/java/com/pinku/SpringBootMongoExampleApplication.java
+++ src/main/java/com/pinku/SpringBootMongoExampleApplication.java
@@ -3,6 +3,8 @@ package com.pinku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://www.youtube.com/watch?v=KJHIR1oSVt8&t=1172s
+
 @SpringBootApplication
 public class SpringBootMongoExampleApplication {
 
